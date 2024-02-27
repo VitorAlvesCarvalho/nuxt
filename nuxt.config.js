0c87dcdc4a784122e672999ab3fda3aeb5c6ca56
VitorAlvesCarvalho <vitor.alves2402@gmail.com>
2024-02-26 21:30:59 -0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,9 +16,6 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  target: 'static',
-
-  cache: true,
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
